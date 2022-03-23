f756cd77eea77cd85884bed822e4e882daab05c6
ThanhDong272 <dongthanh272@gmail.com>
2022-03-23 10:43:50 +0700

    Create bottom navigation and connect to screens.

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import './characters_screen.dart';
+import './deaths_screen.dart';
+import './quotes_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  late List<Map<String, dynamic>> _pages;
+  int _selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    _pages = [
+      {
+        'page': QuotesScreen(),
+        'title': 'Quotes',
+      },
+      {
+        'page': DeathsScreen(),
+        'title': 'Deaths',
+      },
+      {
+        'page': CharactersScreen(),
+        'title': 'Characters',
+      },
+    ];
+
+    super.initState();
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title'] as String),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.orange,
+                Theme.of(context).colorScheme.primary,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+      ),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.black,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.request_quote),
+            label: 'Quotes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Characters',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dangerous_outlined),
+            label: 'Deaths',
+          ),
+        ],
+      ),
+    );
+  }
+}
