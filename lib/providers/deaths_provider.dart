14a04d79814889c3fedfab6126fe3db558a09a6c
Dong Sy Thanh <thanh_ds@vietvang.net>
2022-03-29 15:03:50 +0700

    Finish death screen.

---
+++ lib/providers/deaths_provider.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../configuration/env.dart';
+
+import '../models/death.dart';
+
+class DeathsProvider with ChangeNotifier {
+  List<Death> _deathItems = [];
+
+  List<Death> get items {
+    return [..._deathItems];
+  }
+
+  Future<void> fetchDeaths() async {
+    final url = Uri.parse(Environment.baseUrl + '/deaths');
+
+    try {
+      final response = await http.get(url);
+      final deathData = <Death>[];
+
+      if (response.statusCode == 200) {
+        final deaths = jsonDecode(response.body);
+        for (var death in deaths) {
+          deathData.add(Death.fromJson(death));
+        }
+      }
+
+      _deathItems = deathData;
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> fetchRandomDeath() async {
+    final url = Uri.parse(Environment.baseUrl + '/random-death');
+
+    try {
+      final response = await http.get(url);
+      final deathData = <Death>[];
+
+      if (response.statusCode == 200) {
+        final death = jsonDecode(response.body);
+
+        deathData.add(Death.fromJson(death));
+      }
+
+      _deathItems = deathData;
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
---
+++ lib/widgets/death_list.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/deaths_provider.dart';
+
+import '../widgets/death_item.dart';
+
+class DeathList extends StatelessWidget {
+  const DeathList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final deathData = Provider.of<DeathsProvider>(context);
+    final death = deathData.items;
+
+    return ListView(
+      children: death
+          .map((deathItem) => DeathItem(
+                deathItem.deathId,
+                deathItem.death,
+                deathItem.cause,
+                deathItem.responsible,
+                deathItem.lastWords,
+                deathItem.season,
+                deathItem.episode,
+              ))
+          .toList(),
+    );
+  }
+}
